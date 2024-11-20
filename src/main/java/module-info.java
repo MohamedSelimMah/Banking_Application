ab686284840f1498ab7e8ea414082886b29d96b9
MohamedSelimMah <mohamedselim.mahjoub@iteam-univ.tn>
2024-11-20 21:44:34 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.bankingapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.bankingapplication to javafx.fxml;
+    exports com.example.bankingapplication;
+}
